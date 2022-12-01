3be55f43ced9eb2b9b866a15077a597cabe4d3ab
Brooke Chalmers <breq@breq.dev>
2022-12-01 14:08:52 -0500

    Day 1

---
+++ day1/solution.py
@@ -0,0 +1,12 @@
+with open("input.txt") as f:
+    data = f.readlines()
+
+elves = [0]
+for line in data:
+    if line.strip():
+        elves[-1] += int(line.strip())
+    else:
+        elves.append(0)
+
+# print(max(elves))
+print(sum(sorted(elves, reverse=True)[:3]))
